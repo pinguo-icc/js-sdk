f3074db736eae7d728ef38d93d61e1782ae24b82
komens <youngam@qq.com>
2024-03-08 10:28:23 +0800

    feat(打包): v1.0.6

--- lib/PGBridge/index.d.ts
+++ lib/PGBridge/index.d.ts
@@ -1,14 +1,14 @@
-import { EBridgeType } from './constants';
-import BlurrrIosBridge from './model/BlurrrIosBridge';
-import BlurrrWebBridge from './model/BlurrrWebBridge';
-import { IBridge } from './types/bridge';
+import { EBridgeType } from "./constants";
+import BlurrrIosBridge from "./model/BlurrrIosBridge";
+import BlurrrWebBridge from "./model/BlurrrWebBridge";
+import { IBridge } from "./types/bridge";
 declare class PGBridge {
     static bridgeMap: {
-        'blurrr-ios': typeof BlurrrIosBridge;
-        'blurrr-web': typeof BlurrrWebBridge;
+        "blurrr-ios": typeof BlurrrIosBridge;
+        "blurrr-web": typeof BlurrrWebBridge;
     };
     static EBridgeType: typeof EBridgeType;
     static createBridge(app: string, platform?: string): IBridge;
-    static getPlatform(): "ios" | "web";
+    static getPlatform(): "android" | "ios" | "web";
 }
 export default PGBridge;
