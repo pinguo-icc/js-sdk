2e613abe56ae95642c08de12e4d2862c7c8e3302
komens <youngam@qq.com>
2024-03-08 10:25:30 +0800

    feat(客户端通讯): 优化平台判断方式

--- src/PGBridge/index.ts
+++ src/PGBridge/index.ts
@@ -1,13 +1,13 @@
-import { EBridgeType } from './constants';
-import { BaseBridge } from './model/BaseBridge';
-import BlurrrIosBridge from './model/BlurrrIosBridge';
-import BlurrrWebBridge from './model/BlurrrWebBridge';
-import { IBridge } from './types/bridge';
+import { EBridgeType } from "./constants";
+import { BaseBridge } from "./model/BaseBridge";
+import BlurrrIosBridge from "./model/BlurrrIosBridge";
+import BlurrrWebBridge from "./model/BlurrrWebBridge";
+import { IBridge } from "./types/bridge";
 
 class PGBridge {
   static bridgeMap = {
-    'blurrr-ios': BlurrrIosBridge,
-    'blurrr-web': BlurrrWebBridge,
+    "blurrr-ios": BlurrrIosBridge,
+    "blurrr-web": BlurrrWebBridge,
   };
 
   static EBridgeType = EBridgeType;
@@ -15,19 +15,24 @@ class PGBridge {
   static createBridge(app: string, platform: string = this.getPlatform()): IBridge {
     const bridgeKey = `${app}-${platform}` as ObjectKeys<typeof PGBridge.bridgeMap>;
     const bridgeConstructor = PGBridge.bridgeMap[bridgeKey];
-    if(!bridgeConstructor) {
-      console.error(`未找到${bridgeKey}的Bridge，已使用兜底方案，请进行适配!`)
-      return new BaseBridge()
+    if (!bridgeConstructor) {
+      console.error(`未找到${bridgeKey}的Bridge，已使用兜底方案，请进行适配!`);
+      return new BaseBridge();
     }
     return new bridgeConstructor();
   }
 
   static getPlatform() {
-    if (window.webkit) {
-      // TODO: 这里只能判断是否webkit，等后续有了安卓再判断ios和安卓
-      return 'ios';
-    }
-    return 'web';
+    const ua = window.navigator.userAgent.toLowerCase();
+    // 这里判断是否在内部APP容器中
+    if (window.webkit && /pinguo/.test(ua)) {
+      if (/android/.test(ua)) {
+        return "android";
+      } else if (/iphone|ipad|ipod/.test(ua)) {
+        return "ios";
+      }
+    } // 如果不是统一按照web处理
+    return "web";
   }
 }
 
