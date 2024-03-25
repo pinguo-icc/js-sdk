a9cfcf50fc832bd7d8614d2dfd4c8b68dc374d85
guzhongzhi <guzhongzhi@camera360.com>
2024-03-25 17:15:15 +0800

    add new params for doShare

--- src/PGBridge/common/type.d.ts
+++ src/PGBridge/common/type.d.ts
@@ -1,8 +1,8 @@
 declare type TJson<V = any> = Record<string, V>;
 declare type ObjectKeys<T> = keyof T;
 declare type Rect = {
-    x:number
-    y:number
-    w:number
-    h:number
+    x:Number
+    y:Number
+    w:Number
+    h:Number
 }
